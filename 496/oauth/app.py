7116482197864d7feb6b0f41564592b360224dd4
Andy Kim <kimand@oregonstate.edu>
2018-02-08 16:42:40 -0500

    setup for oauth homework

--- 496/oauth/app.py
+++ 496/oauth/app.py
@@ -6,3 +6,21 @@ import random
 import string
 import json
 import os
+
+CLIENT_ID = '308449516790-6j0f954v4j4rcggnbesda06pme6cbiln.apps.googleusercontent.com'
+CLIENT_SECRET = '_FQyulNpzTTPLMDsFVaH9TI5'
+REDIRECT = 'http://marine-waters-192703.appspot.com/authorization'
+
+class index(webapp2.RequestHandler):
+    def get(self):
+
+class oAuth(webapp2.RequestHandler):
+    def get(self):
+
+app = webapp2.WSGIApplication([
+    ('/', index),
+    ('/authorization', oAuth)
+], debug=True)
+
+
+
