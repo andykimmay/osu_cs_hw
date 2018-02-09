d414b6b8d4638098b5dfe1b9fd0e3b3bb43ce350
Andy Kim <kimand@oregonstate.edu>
2018-02-08 22:56:17 -0500

    small update

--- 496/oauth/app.py
+++ 496/oauth/app.py
@@ -7,12 +7,20 @@ import string
 import json
 import os
 
-CLIENT_ID = '308449516790-bsi5gplq6miq88ra8cpl6j4k8s98ua0h.apps.googleusercontent.com' 
-CLIENT_SECRET = ' 73vRPt_qkMndrIvkybbu5OtL'
+CLIENT_ID = '147678634069-so1aviujnuhabbnlauqf7r6nqmostjic.apps.googleusercontent.com' 
+CLIENT_SECRET = 'RfBXXDxOCKou4_3AZB4SyolJ'
 REDIRECT = 'http://trust-tiger.appspot.com/authorization'
 
 class index(webapp2.RequestHandler):
     def get(self):
+        randomString = 
+
+        url_text = 'Click Here'
+
+        url = "https://accounts.google.com/o/oauth2/v2/auth?scope=email&access_type=offline&include_granted_scopes=true&state="
+        url = url + randomString
+        url = url + "&redirect_uri=https://oauth2-imp.appspot.com/authorize&&response_type=code&client_id="
+        url = url + CLIENT_ID
 
 class oAuth(webapp2.RequestHandler):
     def get(self):
